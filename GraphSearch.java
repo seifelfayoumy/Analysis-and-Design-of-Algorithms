291607ccff47a48f00907a6445c4fca2aea0e0be
seifelfayoumy <49912407+seifelfayoumy@users.noreply.github.com>
2024-01-13 22:41:49 +0200

    Update GraphSearch.java

--- GraphSearch.java
+++ GraphSearch.java
@@ -3,10 +3,13 @@ import java.util.*;
 public class GraphSearch {
     private LinkedList<Integer>[] adj; // Adjacency list
     private int V; // Number of vertices
+    private int[] parent; // To store the parent of a given vertex
+
 
     GraphSearch(int v) {
         V = v + 1; 
         adj = new LinkedList[V];
+        parent = new int[V];
         for (int i = 1; i < V; ++i)
             adj[i] = new LinkedList<>();
     }
@@ -79,6 +82,54 @@ public class GraphSearch {
         }
         return true;
     }
+    
+    void printCycle(int v, int u) {
+        LinkedList<Integer> cycle = new LinkedList<>();
+        cycle.add(u);
+        while (v != u) {
+            cycle.addFirst(v);
+            v = parent[v];
+        }
+        cycle.addFirst(u);
+        System.out.println("Cycle: " + cycle);
+    }
+
+    boolean isCyclicUtil(int v, boolean[] visited, boolean[] recStack) {
+        if (recStack[v]) {
+            return true;
+        }
+
+        if (visited[v]) {
+            return false;
+        }
+
+        visited[v] = true;
+        recStack[v] = true;
+
+        for (int n : adj[v]) {
+            parent[n] = v;
+            if (isCyclicUtil(n, visited, recStack)) {
+                printCycle(n, v);
+                return true;
+            }
+        }
+
+        recStack[v] = false;
+        return false;
+    }
+
+    void isCyclic() {
+        boolean[] visited = new boolean[V];
+        boolean[] recStack = new boolean[V];
+
+        for (int i = 1; i < V; i++) {
+            if (isCyclicUtil(i, visited, recStack)) {
+                System.out.println("Graph contains cycle");
+                return;
+            }
+        }
+        System.out.println("Graph doesn't contain cycle");
+    }
 
     public static void main(String[] args) {
         GraphSearch g = new GraphSearch(4); // 4 vertices
@@ -100,5 +151,7 @@ public class GraphSearch {
             System.out.println("\nGraph is bipartite");
         else
             System.out.println("\nGraph is not bipartite");
+        
+        g.isCyclic();
     }
 }
